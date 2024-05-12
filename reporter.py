8b1f85807a16215556f26cf0b2164194377730bf
colinarelliott <colinarelliott@gmail.com>
2024-05-11 23:15:03 -0400

    added routes for checking input, output and dest dirs

--- reporter.py
+++ reporter.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 import os
 
+inputPath = "input"
+outputPath = "output"
+destination = "destination"
+
 # Python Flask application that serves the web page and handles the API requests
 app = Flask(__name__)
 
@@ -10,9 +14,30 @@ def index():
 
 @app.route('/import')
 def imp():
-    command = "python3 importer.py"
+    command = "python3 importer.py web"
     out = os.popen(command).read()
     return (out)
+
+@app.route('/input')
+def input():
+    inputFiles = os.listdir(inputPath)
+    if (len(inputFiles) == 0):
+        inputFiles = ["No files to process"]
+    return render_template('index.html', files=inputFiles)
+
+@app.route('/output')
+def output():
+    outputFiles = os.listdir(outputPath)
+    if (len(outputFiles) == 0):
+        outputFiles = ["No files in output directory"]
+    return render_template('index.html', files=outputFiles)
+
+@app.route('/destination')
+def dest():
+    destinationFiles = os.listdir(destination)
+    if (len(destinationFiles) == 0):
+        destinationFiles = ["No files in destination directory"]
+    return render_template('index.html', files=destinationFiles)
 
 if __name__ == '__main__':
     app.run()
